c5ac59c6da8eeacd8d1b40e3fcc48fca5ec1ec1b
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-24 14:04:36 +0200

    add chromium download to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,14 @@ FROM public.ecr.aws/lambda/python:3.9
 # Copy all function code to lambda_task_root
 COPY . ${LAMBDA_TASK_ROOT}
 
+# Hack to install chromium dependencies
+RUN yum install -y -q unzip
+RUN yum install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+# Install Chromium
+COPY install-browser.sh /tmp/
+RUN /usr/bin/bash /tmp/install-browser.sh
+
 # Install the function's dependencies using file requirements.txt
 # from your project folder.
 
--- app.py
+++ app.py
@@ -25,10 +25,25 @@ def get_leiden_pararius_page_source(page_number: int):
     time.sleep(10)
     return driver.page_source
 
-#
 def handler(event, context):
 
-    page_source = get_leiden_pararius_page_source(1)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = "/opt/chrome/stable/chrome"
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-tools")
+    chrome_options.add_argument("--no-zygote")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("window-size=2560x1440")
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+    chrome = webdriver.Chrome("/opt/chromedriver/stable/chromedriver", options=chrome_options)
+    chrome.get("https://cloudbytes.dev")
+    
+    page_source = chrome.find_element_by_xpath("//html").text
     
     return {
         'statusCode': 200,
